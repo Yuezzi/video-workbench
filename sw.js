e0682584219c14e4e79c4dda5adb5de497b77746
Yuezzi <3494419359@qq.com>
2026-07-27 17:07:19 +0800

    视频运营工作台 PWA v17 - 含云端同步

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = "video-ops-v17";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ns => Promise.all(ns.filter(n => n !== CACHE).map(n => caches.delete(n))))); self.clients.claim(); });
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).catch(() => c)));
+});
